323cfa8c4a164253e6a179e7dc18e97d3ce5b53d
Alisa <alisabox@mail.ru>
2022-07-04 01:52:51 +0900

    init: create project template

---
+++ apps/blog/src/app/pages/post-list/post-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngblog-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent  {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  
+}
---
+++ apps/blog/src/app/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngblog-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+})
+export class PostComponent  {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  
+}
