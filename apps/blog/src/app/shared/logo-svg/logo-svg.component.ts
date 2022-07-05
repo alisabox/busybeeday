cef1c7af3db977dc30f569e7f9310d0170ac7203
Alisa <alisabox@mail.ru>
2022-07-06 00:24:24 +0900

    feat: style header

--- apps/blog/src/app/shared/logo-svg/logo-svg.component.ts
+++ apps/blog/src/app/shared/logo-svg/logo-svg.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngblog-logo-svg',
   templateUrl: './logo-svg.component.html',
+  styleUrls: ['./logo-svg.component.scss'],
 })
-export class LogoSvgComponent  {
+export class LogoSvgComponent {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor() { }
 
-  
+
 }
