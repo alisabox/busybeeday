d7024bcefb935af378f62b5abf9814a5506afb5c
Alisa <alisabox@mail.ru>
2022-07-06 20:03:16 +0900

    feat: style footer

--- apps/blog/src/app/shared/footer/footer.component.ts
+++ apps/blog/src/app/shared/footer/footer.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngblog-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent  {
+export class FooterComponent {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor() { }
 
-  
+
 }
